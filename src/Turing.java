30d2034902c13628412ae359be958cc72f103a62
milkesha <milkeshaa@yandex.ru>
2019-03-18 23:33:38 +0300

    Turing machine v.1

--- src/Turing.java
+++ src/Turing.java
@@ -10,6 +10,7 @@ public class Turing {
   private String instructions[];
   private String result;
   private char pointer;
+  private int pointerIndex;
 
   public Turing (int instructionsAmount, String word)
   {
@@ -21,6 +22,7 @@ public class Turing {
     }
     this.word = "##" + word + "##";
     this.pointer = this.word.charAt(1);
+    this.pointerIndex = 1;
     this.work();
     System.out.println("Result: " + this.result);
   }
@@ -29,35 +31,51 @@ public class Turing {
   {
     String currentOperation = "q1";
     char newChar;
+    String buffer;
     char[] temp = this.word.toCharArray();
-    for (int i = 1; i < this.word.length() - 1; i++) {
-      for (String instruction: this.instructions) {
-        if (instruction.substring(0, 2).equals(currentOperation)) {
-          if (this.word.charAt(i) == instruction.charAt(3)) {
-            currentOperation = instruction.substring(8, 10);
-            newChar = instruction.charAt(11);
-            temp[i] = newChar;
-            this.result = String.valueOf(temp);
-            if (instruction.charAt(instruction.length() - 1) == 'R') {
-              this.pointer = this.word.charAt(i + 1);
-              break;
-            }
-            if (instruction.charAt(instruction.length() - 1) == 'L') {
-              this.pointer = this.word.charAt(i - 1);
-              break;
-            }
-            if (currentOperation.equals("q0")) {
-              if (this.pointer == '#') {
-                this.result = "0";
+    do {
+      for (int i = this.pointerIndex; i < temp.length - 1; i++) {
+        for (String instruction: this.instructions) {
+          if (instruction.substring(0, 2).equals(currentOperation)) {
+            if (temp[i] == instruction.charAt(3)) {
+              currentOperation = instruction.substring(8, 10);
+              newChar = instruction.charAt(11);
+              if (temp[i] == '#') {
+                if (!(i <= 1)) {
+                  temp[i] = newChar;
+                  buffer = String.valueOf(temp) + "#";
+                  temp = buffer.toCharArray();
+                } else if (newChar == '#') {
+                  temp[i] = newChar;
+                }
+              } else {
+                temp[i] = newChar;
+              }
+              this.result = String.valueOf(temp);
+              if (instruction.charAt(instruction.length() - 1) == 'R') {
+                this.pointer = temp[i + 1];
+                this.pointerIndex = i + 1;
+                break;
+              }
+              if (instruction.charAt(instruction.length() - 1) == 'L') {
+                this.pointer = temp[i - 1];
+                this.pointerIndex = i - 1;
+                i -= 2;
+                break;
+              }
+              if (currentOperation.equals("q0")) {
+                if (this.pointer == '#') {
+                  this.result = "0";
+                  return;
+                }
                 return;
               }
-              return;
+              break;
             }
-            break;
           }
         }
       }
-    }
+    } while (!currentOperation.equals("q0"));
   }
 
   public static void main(String[] args) {
